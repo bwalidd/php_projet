40ac17c2d9d82a8cbdc64fecc980e6729fce27c5
bwalidd <bouwachmawalid1@gmail.com>
2021-10-29 23:52:21 +0100

    third-commit

--- config/config.php
+++ config/config.php
@@ -3,9 +3,9 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "121212aa");
-define("DB_NAME", "talkingspace");
+define("DB_NAME", "est");
 
-define("SITE_TITLE", "Welcome To TalkingSpace!");
+define("SITE_TITLE", "Welcome To ecole superiere de technologie!");
 
 //Paths
-define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/talkingspace/');
+define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/projet_forum/');
