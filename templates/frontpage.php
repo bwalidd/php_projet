a9d5dfe040cc3c5769c6336f388bed5c142c617b
bwalidd <bouwachmawalid1@gmail.com>
2021-11-06 19:32:23 +0100

    fourth-commit

---
+++ templates/frontpage.php
@@ -0,0 +1,90 @@
+<?php include('includes/header.php'); ?>		
+		<ul id="topics">
+							<li class="topic">
+							<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.jpg" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">le cinqième topic</a></h3>
+									<div class="topic-info">
+										<a href="category.html">Génie agroenvironement</a> >> <a href="profile.html">user_walid</a> >> Posted on: october 16, 2021
+										<span class="badge pull-right">3</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.jpg" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">le quatrième topic</a> </h3>
+									<div class="topic-info">
+										<a href="category.html">génie agroenvironement </a> >> <a href="profile.html">user_salah</a> >> Posted on: October 14, 2021
+										<span class="badge pull-right">7</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.jpg" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">le troisième topic</a></h3>
+									<div class="topic-info">
+										<a href="category.html">Génie mecanique </a> >> <a href="profile.html">user_achraf</a> >> Posted on: October 13, 2021
+										<span class="badge pull-right">4</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.jpg" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">le dexième topic</a></h3>
+									<div class="topic-info">
+										<a href="category.html">génie informatique</a> >> <a href="profile.html">user_user1</a> >> Posted on: October 12, 2021
+										<span class="badge pull-right">2</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.jpg" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">le premier topic</a></h3>
+									<div class="topic-info">
+										<a href="category.html">Génie agroenvironement </a> >> <a href="profile.html">user_walid</a> >> Posted on: October 12, 2021
+										<span class="badge pull-right">4</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+						</ul>
+						<h3>Forum Statistics</h3>
+					<ul>
+						<li>Total Number of Users: <strong>3</strong></li>
+						<li>Total Number of Topics: <strong>5</strong></li>
+						<li>Total Number of Categories: <strong>5</strong></li>
+					</ul>
+<?php include('includes/footer.php'); ?>	
